20d4904b347d2bb9d3106a71362d170af293b96a
neocrimson17 <neo_crimson17@yahoo.com>
2015-01-30 00:00:28 -0500

    the initial files  for the game
    it contains the background image and a working game that should load the
    game table

---
+++ html5 games/js/game.js
@@ -0,0 +1,27 @@
+var canvasBg = document.getElementById('canvasBg');
+var ctxBg = canvasBg.getContext('2d');
+var clearCanvasBtn = document.getElementById('clearCanvasBtn');
+clearCanvasBtn.addEventListener('click',clearCanvas,false);
+
+var gameWidth = canvasBg.width;
+var gameHeight = canvasBg.height;
+
+var imgSprite = new Image();
+imgSprite.src = 'images/TTBOARD.png';
+imgSprite.addEventListener('load',drawBg,false);
+// srcX and srcY are the initial coordinates of the background image, usually (0,0)
+// the background will be drawn from srcX and srcY to gameWidth and gameHeight
+// this is the dimension of the background image
+// drawX and drawY are coordinates to tell where to begin to the sprite and gameWidth and gameHeight
+// are coordinates where it will end.  
+function drawBg(){
+	var srcX = 0;
+	var srcY = 0;
+	var drawX = 0;
+	var drawY = 0;
+	ctxBg.drawImage(imgSprite,srcX,srcY,gameWidth,gameHeight,drawX,drawY,gameWidth,gameHeight);
+}
+
+function clearCanvas(){
+	ctxBg.clearRect(0,0,722,762);
+}
