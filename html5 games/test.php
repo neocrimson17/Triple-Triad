a31ea62adfc5271a88f5b458a92643a1de21412f
neocrimson17 <neo_crimson17@yahoo.com>
2015-04-27 14:35:30 -0400

    test.php successfully connected to the database
    the first card's name is correctly displayed and as well as the top
    value. "Geezard, 1"

--- html5 games/test.php
+++ html5 games/test.php
@@ -1,7 +1,27 @@
 <?php
 
-   echo json_encode($_POST['monsterId']);
-   //echo(rand(10,100));
-     //echo alert("WORKS!");
+  
+	//$q = ($_POST['monsterId']);
+	//echo($q);
+	//echo json_encode($_POST['monsterId']);
+	
+	$card_array = json_decode($_POST['monsterId']);
+	
+	$first_card = $card_array[0];
+	//echo json_encode($first_card);
+	
+	echo json_encode(array($first_card->name, $first_card->top));
+	
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+  
+	// Create connection
+	$conn = new mysqli($servername, $username, $password);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	//echo "Connected successfully";
 
 ?>
